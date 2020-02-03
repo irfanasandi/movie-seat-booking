bbb9f80bf0b0741faed0fa3b46e11786a1f4f643
Irfana Sandi <irfanasandi21@gmail.com>
2020-02-04 06:22:35 +0700

    well done

---
+++ script.js
@@ -0,0 +1,61 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+
+const movieSelect = document.getElementById("movie");
+let ticketPrice = movieSelect.value;
+
+populateUI();
+
+function updateSelectedCount() {
+  const selected = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selected].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+  const selectedSeatCount = selected.length;
+  count.innerText = selectedSeatCount;
+  total.innerText = selectedSeatCount * ticketPrice;
+}
+
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  movieSelect.selectedIndex = selectedMovieIndex;
+}
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+//movie select event
+movieSelect.addEventListener("change", e => {
+  ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+//Seat select event
+container.addEventListener("click", e => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+    updateSelectedCount();
+  }
+});
+
+updateSelectedCount();
